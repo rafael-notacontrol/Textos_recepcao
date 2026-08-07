dc839a45a11850544b839eb1d85bc63a69401f9c
rafael-notacontrol <rafael.matriz@notacontrol.com.br>
2026-08-07 14:57:55 +0000

    importação do coponente Frase do dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Calendario from "./components/Calendario";
+import FraseDoDia from "./components/Frases";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
 import TextosRecepcao from "./components/TextosRecepcao";
