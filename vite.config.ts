61d649a2eb131c8a1d44d2ba1db5c1795f251665
rafael-notacontrol <rafael.matriz@notacontrol.com.br>
2026-08-17 17:25:02 +0000

    Transformando em PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,31 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Textos Recepção',
+        short_name: 'Recepção',
+        description: 'Textos de recepção de chamados',
+        theme_color: '#2563EB',
+        background_color: '#F5F7FB',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: '/cup.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/cup.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
